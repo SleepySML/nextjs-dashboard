b64fb6d58cdf721bbf6c46bf83eb9f4ccd5fd831
Evgenii <evgenij.sleepy@gmail.com>
2024-03-15 15:53:44 +0300

    chapters 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
+import {signIn} from "@/app/auth";
 
 const FormSchema = z.object({
     id: z.string(),
@@ -18,7 +20,7 @@ const FormSchema = z.object({
       }),
     date: z.string(),
   });
-  
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
@@ -63,7 +65,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
- 
+
 export async function updateInvoice(
     id: string,
     prevState: State,
@@ -74,17 +76,17 @@ export async function updateInvoice(
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
-   
+
     if (!validatedFields.success) {
       return {
         errors: validatedFields.error.flatten().fieldErrors,
         message: 'Missing Fields. Failed to Update Invoice.',
       };
     }
-   
+
     const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
-   
+
     try {
       await sql`
         UPDATE invoices
@@ -94,7 +96,7 @@ export async function updateInvoice(
     } catch (error) {
       return { message: 'Database Error: Failed to Update Invoice.' };
     }
-   
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
@@ -110,3 +112,22 @@ export async function deleteInvoice(id: string) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
 }
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
